252a70797fa7a15f5737b94b66d70b377295ab6a
aloksamyotech <127102691+aloksamyotech@users.noreply.github.com>
2023-08-02 17:52:57 +0530

    a

--- components/home/Homepage.tsx
+++ components/home/Homepage.tsx
@@ -5,15 +5,10 @@ import Banner from "./Banner";
 const Homepage = () => {
   return (
     <>
-<<<<<<< HEAD
-      <Header />
-      <Footerbox/>
-=======
       <div className="main-wrp">
           <Header />
           <Banner />
       </div>
->>>>>>> ef691387ea8871ba59b0bfd341d166b91febb7db
     </>
   );
 };
