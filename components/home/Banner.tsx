1ae72a60d1ff496329893a1fd37be14c4649d1c1
harshitmehta93 <harshit.mehta@proaxiom.com>
2023-08-02 16:14:12 +0530

    header changes

--- components/home/Banner.tsx
+++ components/home/Banner.tsx
@@ -5,7 +5,7 @@ const Banner = () => {
         <div className="banner-section container">
 
         <div className="row">
-            <div className="col-md-6 col-xl-6 col-sm-12">
+            <div className="col-md-6 col-xl-6 col-sm-12 banner-content">
                     <div className="banner-content">
                         <h2>Make your <span>own</span> signature <span>style</span></h2>
                         <p className="common-content">Lorem ipsum dolor sit amet consectetur. Id rhoncus a curabitur massa aliquet ipsum velit convallis lacus. Orci magna enim arcu orci amet.</p>
