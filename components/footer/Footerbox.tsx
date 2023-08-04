67ed5c81108a6d3926540c5c5fd36d2680ddace7
mohitdev467 <mohitdev467@gmail.com>
2023-08-04 16:30:35 +0530

    footer-main-pricing section changes

--- components/footer/Footerbox.tsx
+++ components/footer/Footerbox.tsx
@@ -1,96 +1,88 @@
-import React from 'react'
-import "./Footerbox.css"
+import React from "react";
+import "./Footerbox.css";
 const Footerbox = () => {
   return (
-    <div className='footer-container'>
-      <div className='footer-container-inner'>
-        <div className='footer-logo'>
+    <div className="footer-container">
+      <div className="footer-container-inner">
+        <div className="footer-logo">
           <img src="/logo.svg" />
         </div>
-        <div className='footer-socials'>
-          <div className='footer-quick'><span> Quick Links</span></div>
-          <div className='footer-stay'><span> Stay updated</span></div>
-          <div className='footer-icons'>
-            <div>
-              <img src="/linkdin.svg" alt="linkedlin" className='footer-social-icons-img' />
-            </div>
-            <div>
-
-              <img src="/facebook.svg" alt="linkedlin" className='footer-social-icons-img' />
-            </div>
-            <div>
-
-              <img src="/twitter.svg" alt="linkedlin" className='footer-social-icons-img' />
-            </div>
-
+        <div className="footer-socials">
+          <div className="footer-quick">
+            <span> Quick Links</span>
           </div>
-        </div>
-      </div>
-      <div className='footer-body'>
-        <div className='footer-body-title'>
-          <span>Lorem ipsum dolor sit amet consectetur. Luctus tincidunt neque quis sit tristique. Elementum a vitae augue condimentum augue ipsum nec. </span>
-        </div>
-        <div className='footer-body-list'>
-          <span>
-            Home
-          </span>
-          <span>
-            Contact
-          </span>
-          <span>
-            FAQs
-          </span>
-          <span>
-            Pricing
-          </span>
-          <span>
-            Documentation
-          </span>
-        </div>
-        <div className='footer-body-list-etsy'>
-          <span>
-            Etsy
-          </span>
-          <span>
-            Walmart
-          </span>
-          <span>
-            eBay
-          </span>
-          <span>
-            Lorem
-          </span>
-        </div>
-        <div >
-          <span className='footer-input-title'>Please enter your email</span>
-          <div>
-            <div className='footer-input'>
-              <input type='email' placeholder='Email' />
-              <button>SEND</button>
+          <div className="footer-stay">
+            <span> Stay updated</span>
+          </div>
+          <div className="footer-icons">
+            <div>
+              <img
+                src="/linkdin.svg"
+                alt="linkedlin"
+                className="footer-social-icons-img"
+              />
+            </div>
+            <div>
+              <img
+                src="/facebook.svg"
+                alt="linkedlin"
+                className="footer-social-icons-img"
+              />
+            </div>
+            <div>
+              <img
+                src="/twitter.svg"
+                alt="linkedlin"
+                className="footer-social-icons-img"
+              />
             </div>
           </div>
         </div>
       </div>
-      <div className='footer-copyryt'>
-        <p className='footer-copyryt-title'>
-          <span><img src='/Vector.svg' alt='copy' className='footer-copyryt-img' /></span>
+      <div className="footer-body">
+        <div className="footer-body-title">
+          <span>
+            Lorem ipsum dolor sit amet consectetur. Luctus tincidunt neque quis
+            sit tristique. Elementum a vitae augue condimentum augue ipsum nec.{" "}
+          </span>
+        </div>
+        <div className="footer-body-list">
+          <span>Home</span>
+          <span>Contact</span>
+          <span>FAQs</span>
+          <span>Pricing</span>
+          <span>Documentation</span>
+        </div>
+        <div className="footer-body-list-etsy">
+          <span>Etsy</span>
+          <span>Walmart</span>
+          <span>eBay</span>
+          <span>Lorem</span>
+        </div>
+        <div className="footer-email-section">
+          <span className="footer-input-title">Please enter your email</span>
+          <input type="email" placeholder="Email" className="footer-input" />
+          <button className="footer-button">SEND</button>
+        </div>
+      </div>
+      <div className="footer-copyryt">
+        <p className="footer-copyryt-title">
+          <span>
+            <img
+              src="/copyrightVector.svg"
+              alt="copy"
+              className="footer-copyryt-img"
+            />
+          </span>
           2023 PODfusion. All rights preserved.
         </p>
-        <p className='footer-copyryt-subs'>
-          Intellectual Property Policy
-        </p>
-        <p className='footer-copyryt-subs'>
-          Terms of service
-        </p>
-        <p className='footer-copyryt-subs'>
-          Privacy Policy
-        </p>
-        <p className='footer-copyryt-subs'>
-          Security
-        </p>
+        <p className="footer-copyryt-subs">Intellectual Property Policy</p>
+        <p className="footer-copyryt-subs">Terms of service</p>
+        <p className="footer-copyryt-subs">Privacy Policy</p>
+        <p className="footer-copyryt-subs">Security</p>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footerbox
+export default Footerbox;
--- components/home/SubscriptionPlan.tsx
+++ components/home/SubscriptionPlan.tsx
@@ -1,226 +1,226 @@
-"use client"
-import React from 'react'
-import './subscriptionPlan.css'
-import Image from '../../public/flat-color-icons_settings.png'
+"use client";
+import React from "react";
+import "./subscriptionPlan.css";
 
 const SubscriptionPlan = () => {
+  const subscriptionData = [
+    {
+      key: 0,
+      currency: "$",
+      price: 0,
+      period: "Monthly",
+      planType: "Free",
+      title1: "5 daily tasks ( Uplaod, Design, Product, etc...)",
+      title2: "No need to download anything, it runs completely in the cloud.",
+      title3:
+        "Lorem ipsum dolor sit amet consectetur. Consequat pellentesque magna et amet.",
+      title4: "",
+      title5: "",
+      title6: "",
+      title7: "",
+    },
+    {
+      key: 1,
+      currency: "$",
+      price: 9.99,
+      period: "Monthly",
+      planType: "Premium",
+      title1: "5 daily tasks ( Uplaod, Design, Product, etc...)",
+      title2: "Access to all marketplaces",
+      title3: "Basic support",
+      title4: "No need to download anything, it runs completely in the cloud.",
+      title5: "",
+      title6: "",
+      title7: "",
+    },
+    {
+      key: 2,
+      currency: "$",
+      price: 19.99,
+      period: "Monthly",
+      planType: "Pro",
+      title1: "150 Daily tasks ( Upload, Design,Product.. etc.)",
+      title2:
+        "In a post-apocalyptic world, a determined protagonist embarks on a perilous journey, battling enemies and forging unlikely alliances in search of sanctuary.",
+      title3: "3 days guarantee support",
+      title4: "Automatically check your listing for trademark infringement",
+      title5: "One more: Access to all marketplaces",
+      title6: "",
+      title7: "",
+    },
+    {
+      key: 3,
+      currency: "$",
+      price: 49.99,
+      period: "Monthly",
+      planType: "Enterprise",
+      title1: "Unlimited daily tasks",
+      title2:
+        "Boost productivity and quality with production automation's efficiency and precision",
+      title3: "API Access",
+      title4: "Automatically check your listing for trademark infringement",
+      title5: "One more: Access to all marketplaces",
+      title6: "No need to download anything, runs completely in the cloud",
+      title7: "24h support",
+    },
+  ];
 
-    const subscriptionData = [
-        {
-            key: 0,
-            currency: "$",
-            price: 0,
-            period: "Monthly",
-            planType: "Free",
-            title1: "5 daily tasks ( Uplaod, Design, Product, etc...)",
-            title2: "No need to download anything, it runs completely in the cloud.",
-            title3: "Lorem ipsum dolor sit amet consectetur. Consequat pellentesque magna et amet.",
-            title4: "",
-            title5: "",
-            title6: "",
-            title7: ""
-        },
-        {
-            key: 1,
-            currency: "$",
-            price: 9.99,
-            period: "Monthly",
-            planType: "Premium",
-            title1: "5 daily tasks ( Uplaod, Design, Product, etc...)",
-            title2: "Access to all marketplaces",
-            title3: "Basic support",
-            title4: "No need to download anything, it runs completely in the cloud.",
-            title5: "",
-            title6: "",
-            title7: ""
-        },
-        {
-            key: 2,
-            currency: "$",
-            price: 19.99,
-            period: "Monthly",
-            planType: "Pro",
-            title1: "150 Daily tasks ( Upload, Design,Product.. etc.)",
-            title2: "In a post-apocalyptic world, a determined protagonist embarks on a perilous journey, battling enemies and forging unlikely alliances in search of sanctuary.",
-            title3: "3 days guarantee support",
-            title4: "Automatically check your listing for trademark infringement",
-            title5: "One more: Access to all marketplaces",
-            title6: "",
-            title7: ""
-        },
-        {
-            key: 3,
-            currency: "$",
-            price: 49.99,
-            period: "Monthly",
-            planType: "Enterprise",
-            title1: "Unlimited daily tasks",
-            title2: "Boost productivity and quality with production automation's efficiency and precision",
-            title3: "API Access",
-            title4: "Automatically check your listing for trademark infringement",
-            title5: "One more: Access to all marketplaces",
-            title6: "No need to download anything, runs completely in the cloud",
-            title7: "24h support"
-        },
-    ]
-
-
-    return (
-        <div className='subscription-plan-container'>
-            <div className="container">
-                <div className="row main-pricing">
-                    <div className="col-md-12 col-xl-12 col-sm-12">
-                        <h3>
-                            Choose the Plan that Fits Your Business              </h3>
-
-                    </div>
-
-                    <p>Lorem ipsum dolor sit amet consectetur. Id vitae semper sed risus.</p>
-                </div>
-            </div>
-            <div className='container-fluid'>
-                {/* <div className='subscription-cards-wrapper'>
-                    {
-                        subscriptionData.map((data, index) => {
-                            return (
-                                <div className='subscription-card-inner-wrapper' key={data.key}>
-                                    <div className='subscription-card-contents'>
-                                        <div className='frame-59'>
-                                            <span className='currency'>{data.currency}</span>
-                                            <span className="price">
-                                                {data.price}
-                                            </span>
-                                            <span className='period'>{data.period}</span>
-                                        </div>
-                                        <div className='subscription-types'>
-                                            {data.planType}
-                                        </div>
-
-                                    </div>
-                                    <div className='subscription-description'>
-                                        <ul>
-                                            <li>{data.title1}
-                                            </li>
-                                            <li>{data.title2}
-                                            </li>
-                                            <li>{data.title3}
-                                            </li>
-                                            <li>{data.title4}
-                                            </li>
-                                            <li>{data.title5}
-                                            </li>
-                                            <li>{data.title6}
-                                            </li>
-                                            <li>{data.title7}
-                                            </li>
-                                        </ul>
-                                    </div>
-
-                                    <div className='subscription-buy-button'>
-                                        <button className='btn'>
-                                            <span>Try now</span>
-                                        </button>
-                                    </div>
-                                </div>
-
-                            )
-                        })
-                    }
-                   
-                </div> */}
-
-                <div className="row">
-                    <div className="col-md-3 col-sm-6 col-xs-12">
-                        <div className="pricingTable">
-                            <div className="pricingTable-header">
-                                <span className="currency">$</span>
-                                <span className="amount">9.99</span>
-                                <span className="duration">Monthly</span>
-                                <div className="price-value">
-                                    <span>Free</span>
-                                    <span className='line-1'><hr /></span>
-                                </div>
-                            </div>
-                            <ul className="pricing-content">
-                                <li>50GB Disk Space</li>
-                                <li>50 Email Accounts</li>
-                                <li>50GB Bandwidth</li>
-                                <li className="disable">Maintenance</li>
-                                <li className="disable">15 Subdomains</li>
-                            </ul>
-                            {/* <div className="pricingTable-signup">
-                                <a href="#">Sign Up</a>
-                            </div> */}
-                        </div>
-                    </div>
-                    <div className="col-md-3 col-sm-6 col-xs-12">
-                        <div className="pricingTable green">
-                            <div className="pricingTable-header">
-                                <h3 className="title">Business</h3>
-                            </div>
-                            <div className="price-value">
-                                <span className="amount">$19.99</span>
-                                <span className="duration">per month</span>
-                            </div>
-                            <ul className="pricing-content">
-                                <li>50GB Disk Space</li>
-                                <li>50 Email Accounts</li>
-                                <li>50GB Bandwidth</li>
-                                <li>Maintenance</li>
-                                <li className="disable">15 Subdomains</li>
-                            </ul>
-                            <div className="pricingTable-signup">
-                                <a href="#">Sign Up</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-md-3 col-sm-6 col-xs-12">
-                        <div className="pricingTable green">
-                            <div className="pricingTable-header">
-                                <h3 className="title">Business</h3>
-                            </div>
-                            <div className="price-value">
-                                <span className="amount">$19.99</span>
-                                <span className="duration">per month</span>
-                            </div>
-                            <ul className="pricing-content">
-                                <li>50GB Disk Space</li>
-                                <li>50 Email Accounts</li>
-                                <li>50GB Bandwidth</li>
-                                <li>Maintenance</li>
-                                <li className="disable">15 Subdomains</li>
-                            </ul>
-                            <div className="pricingTable-signup">
-                                <a href="#">Sign Up</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-md-3 col-sm-6 col-xs-12">
-                        <div className="pricingTable green">
-                            <div className="pricingTable-header">
-                                <h3 className="title">Business</h3>
-                            </div>
-                            <div className="price-value">
-                                <span className="amount">$19.99</span>
-                                <span className="duration">per month</span>
-                            </div>
-                            <ul className="pricing-content">
-                                <li>50GB Disk Space</li>
-                                <li>50 Email Accounts</li>
-                                <li>50GB Bandwidth</li>
-                                <li>Maintenance</li>
-                                <li className="disable">15 Subdomains</li>
-                            </ul>
-                            <div className="pricingTable-signup">
-                                <a href="#">Sign Up</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="subscription-plan-container">
+      <div className="container">
+        <div className="row main-pricing">
+          <div className="col-md-12 col-xl-12 col-sm-12">
+            <h3>Choose the Plan that Fits Your Business</h3>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur. Id vitae semper sed risus.
+          </p>
         </div>
+      </div>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-3 col-sm-6 col-xs-12">
+            <div className="pricingTable">
+              <div className="pricingTable-header">
+                <span className="currency">$</span>
+                <span className="amount">0</span>
+                <span className="duration">Monthly</span>
+                <div className="price-value">
+                  <span>Free</span>
+                  <span className="line-1">
+                    <hr />
+                  </span>
+                </div>
+              </div>
+              <ul className="pricing-content">
+                <li>5 daily tasks ( Uplaod, Design, Product, etc...)</li>
+                <li>
+                  No need to download anything, it runs completely in the cloud.
+                </li>
+                <li>
+                  Lorem ipsum dolor sit amet consectetur. Consequat pellentesque
+                  magna et amet.
+                </li>
+              </ul>
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <div className="pricingTable-signup">
+                <button className="try-now-btn">Try Now</button>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3 col-sm-6 col-xs-12">
+            <div className="pricingTable">
+              <div className="pricingTable-header">
+                <span className="currency">$</span>
+                <span className="amount">9.99</span>
+                <span className="duration">Monthly</span>
+                <div className="price-value-premium">
+                  <span>Premium</span>
+                  <span className="line-1">
+                    <hr />
+                  </span>
+                </div>
+              </div>
+              <ul className="pricing-content-second-child">
+                <li>5 daily tasks ( Uplaod, Design, Product, etc...)</li>
+                <li>Access to all marketplaces</li>
+                <li>Basic support</li>
+                <li>
+                  No need to download anything, it runs completely in the cloud.
+                </li>
+              </ul>
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
 
-    )
-}
+              <div className="pricingTable-signup-premium">
+                <button className="try-now-btn-premium">Try Now</button>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3 col-sm-6 col-xs-12">
+            <div className="pricingTable">
+              <div className="pricingTable-header">
+                <span className="currency">$</span>
+                <span className="amount-pro">19.99</span>
+                <span className="duration">Monthly</span>
+                <div className="price-value-pro">
+                  <span>Pro</span>
+                  <span className="best-seller">
+                    <p>Best Seller</p>
+                  </span>
+                  <span className="line-1">
+                    <hr />
+                  </span>
+                </div>
+              </div>
+              <ul className="pricing-content-third-child">
+                <li>150 Daily tasks ( Upload, Design,Product.. etc.)</li>
+                <li>
+                  In a post-apocalyptic world, a determined protagonist embarks
+                  on a perilous journey, battling enemies and forging unlikely
+                  alliances in search of sanctuary.
+                </li>
+                <li>3 days guarantee support</li>
+                <li>
+                  Automatically check your listing for trademark infringement
+                </li>
+                <li>One more: Access to all marketplaces</li>
+              </ul>
+              <br />
+              <br />
 
-export default SubscriptionPlan
+              <div className="pricingTable-signup-pro">
+                <button className="try-now-btn-pro">Try Now</button>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3 col-sm-6 col-xs-12">
+            <div className="pricingTable">
+              <div className="pricingTable-header">
+                <span className="currency">$</span>
+                <span className="amount">49.99</span>
+                <span className="duration">Monthly</span>
+                <div className="price-value-enterprise">
+                  <span>Enterprise</span>
+                  <span className="line-1">
+                    <hr />
+                  </span>
+                </div>
+              </div>
+              <ul className="pricing-content-fourth-child">
+                <li>Unlimited daily tasks</li>
+                <li>
+                  Boost productivity and quality with production automation`s
+                  efficiency and precision
+                </li>
+                <li>API Access</li>
+                <li>
+                  Automatically check your listing for trademark infringement
+                </li>
+                <li>One more: Access to all marketplaces</li>
+                <li>
+                  No need to download anything, runs completely in the cloud
+                </li>
+                <li>24h support</li>
+              </ul>
+              <br />
+
+              <div className="pricingTable-signup-enterprise">
+                <button className="try-now-btn-enterprise">Try Now</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SubscriptionPlan;
