fc328d48527ff5b49c445ff598f54198b3c514b9
mohitdev467 <mohitdev467@gmail.com>
2023-08-07 19:14:33 +0530

    fixed-some-spacing-bugs

--- components/home/SubscriptionPlan.tsx
+++ components/home/SubscriptionPlan.tsx
@@ -76,131 +76,145 @@ const SubscriptionPlan = () => {
             Lorem ipsum dolor sit amet consectetur. Id vitae semper sed risus.
           </p>
         </div>
+
+        <div className="toggle-wrapper">
+          <span className="toggle-inner"></span>
+        </div>
       </div>
       <div className="container">
         <div className="row pricing-wrapper">
-          <div className="col-md-3 col-sm-6 col-xs-12">
+          <div className="col-md-3 col-sm-6 col-xs-12 ">
             <div className="pricingTable">
-              <div className="pricingTable-header">
-                <span className="currency">$</span>
-                <span className="amount">0</span>
-                <span className="duration">Monthly</span>
-                <div className="price-value">
-                  <span>Free</span>
-                  <span className="line-1">
-                    <hr />
-                  </span>
+              <div className="bg-ellipse">
+                <div className="pricingTable-header">
+                  <span className="currency">$</span>
+                  <span className="amount">0</span>
+                  <span className="duration">Monthly</span>
+                  <div className="price-value">
+                    <span>Free</span>
+                    <span className="line-1">
+                      <hr />
+                    </span>
+                  </div>
                 </div>
-              </div>
-              <ul className="pricing-content">
-                <li>5 daily tasks ( Uplaod, Design, Product, etc...)</li>
-                <li>
-                  No need to download anything, it runs completely in the cloud.
-                </li>
-                <li>
-                  Lorem ipsum dolor sit amet consectetur. Consequat pellentesque
-                  magna et amet.
-                </li>
-              </ul>
-              <div className="pricingTable-signup">
-                <button className="try-now-btn">Try Now</button>
+                <ul className="pricing-content">
+                  <li>5 daily tasks ( Uplaod, Design, Product, etc...)</li>
+                  <li>
+                    No need to download anything, it runs completely in the
+                    cloud.
+                  </li>
+                  <li>
+                    Lorem ipsum dolor sit amet consectetur. Consequat
+                    pellentesque magna et amet.
+                  </li>
+                </ul>
+                <div className="pricingTable-signup">
+                  <button className="try-now-btn">Try Now</button>
+                </div>
               </div>
             </div>
           </div>
           <div className="col-md-3 col-sm-6 col-xs-12">
             <div className="pricingTable">
-              <div className="pricingTable-header">
-                <span className="currency">$</span>
-                <span className="amount">9.99</span>
-                <span className="duration">Monthly</span>
-                <div className="price-value-premium">
-                  <span>Premium</span>
-                  <span className="line-1">
-                    <hr />
-                  </span>
+              <div className="bg-ellipse-premium">
+                <div className="pricingTable-header">
+                  <span className="currency">$</span>
+                  <span className="amount">9.99</span>
+                  <span className="duration">Monthly</span>
+                  <div className="price-value-premium">
+                    <span>Premium</span>
+                    <span className="line-1">
+                      <hr />
+                    </span>
+                  </div>
                 </div>
-              </div>
-              <ul className="pricing-content-second-child">
-                <li>5 daily tasks ( Uplaod, Design, Product, etc...)</li>
-                <li>Access to all marketplaces</li>
-                <li>Basic support</li>
-                <li>
-                  No need to download anything, it runs completely in the cloud.
-                </li>
-              </ul>
+                <ul className="pricing-content-second-child">
+                  <li>5 daily tasks ( Uplaod, Design, Product, etc...)</li>
+                  <li>Access to all marketplaces</li>
+                  <li>Basic support</li>
+                  <li>
+                    No need to download anything, it runs completely in the
+                    cloud.
+                  </li>
+                </ul>
 
-              <div className="pricingTable-signup-premium">
-                <button className="try-now-btn-premium">Try Now</button>
+                <div className="pricingTable-signup-premium">
+                  <button className="try-now-btn-premium">Try Now</button>
+                </div>
               </div>
             </div>
           </div>
           <div className="col-md-3 col-sm-4 col-xs-12">
-            <div className="pricingTable">
-              <div className="pricingTable-header">
-                <span className="currency">$</span>
-                <span className="amount-pro">19.99</span>
-                <span className="duration">Monthly</span>
-                <div className="price-value-pro">
-                  <span>Pro</span>
-                  <span className="best-seller">
-                    <p>Best Seller</p>
-                  </span>
-                  <span className="line-1">
-                    <hr />
-                  </span>
+            <div className="pricingTable" id="pro-table">
+              <div className="bg-ellipse-pro">
+                <div className="pricingTable-header">
+                  <span className="currency">$</span>
+                  <span className="amount-pro">19.99</span>
+                  <span className="duration">Monthly</span>
+                  <div className="price-value-pro">
+                    <span>Pro</span>
+                    <span className="best-seller">
+                      <p>Best Seller</p>
+                    </span>
+                    <span className="line-1">
+                      <hr />
+                    </span>
+                  </div>
                 </div>
-              </div>
-              <ul className="pricing-content-third-child">
-                <li>150 Daily tasks ( Upload, Design,Product.. etc.)</li>
-                <li>
-                  In a post-apocalyptic world, a determined protagonist embarks
-                  on a perilous journey, battling enemies and forging unlikely
-                  alliances in search of sanctuary.
-                </li>
-                <li>3 days guarantee support</li>
-                <li>
-                  Automatically check your listing for trademark infringement
-                </li>
-                <li>One more: Access to all marketplaces</li>
-              </ul>
+                <ul className="pricing-content-third-child">
+                  <li>150 Daily tasks ( Upload, Design,Product.. etc.)</li>
+                  <li>
+                    In a post-apocalyptic world, a determined protagonist
+                    embarks on a perilous journey, battling enemies and forging
+                    unlikely alliances in search of sanctuary.
+                  </li>
+                  <li>3 days guarantee support</li>
+                  <li>
+                    Automatically check your listing for trademark infringement
+                  </li>
+                  <li>One more: Access to all marketplaces</li>
+                </ul>
 
-              <div className="pricingTable-signup-pro">
-                <button className="try-now-btn-pro">Try Now</button>
+                <div className="pricingTable-signup-pro">
+                  <button className="try-now-btn-pro">Try Now</button>
+                </div>
               </div>
             </div>
           </div>
           <div className="col-md-3 col-sm-6 col-xs-12">
             <div className="pricingTable">
-              <div className="pricingTable-header">
-                <span className="currency">$</span>
-                <span className="amount">49.99</span>
-                <span className="duration">Monthly</span>
-                <div className="price-value-enterprise">
-                  <span>Enterprise</span>
-                  <span className="line-1">
-                    <hr />
-                  </span>
+              <div className="bg-ellipse-enterprise">
+                <div className="pricingTable-header">
+                  <span className="currency">$</span>
+                  <span className="amount">49.99</span>
+                  <span className="duration">Monthly</span>
+                  <div className="price-value-enterprise">
+                    <span>Enterprise</span>
+                    <span className="line-1">
+                      <hr />
+                    </span>
+                  </div>
                 </div>
-              </div>
-              <ul className="pricing-content-fourth-child">
-                <li>Unlimited daily tasks</li>
-                <li>
-                  Boost productivity and quality with production automation`s
-                  efficiency and precision
-                </li>
-                <li>API Access</li>
-                <li>
-                  Automatically check your listing for trademark infringement
-                </li>
-                <li>One more: Access to all marketplaces</li>
-                <li>
-                  No need to download anything, runs completely in the cloud
-                </li>
-                <li>24h support</li>
-              </ul>
+                <ul className="pricing-content-fourth-child">
+                  <li>Unlimited daily tasks</li>
+                  <li>
+                    Boost productivity and quality with production automation`s
+                    efficiency and precision
+                  </li>
+                  <li>API Access</li>
+                  <li>
+                    Automatically check your listing for trademark infringement
+                  </li>
+                  <li>One more: Access to all marketplaces</li>
+                  <li>
+                    No need to download anything, runs completely in the cloud
+                  </li>
+                  <li>24h support</li>
+                </ul>
 
-              <div className="pricingTable-signup-enterprise">
-                <button className="try-now-btn-enterprise">Try Now</button>
+                <div className="pricingTable-signup-enterprise">
+                  <button className="try-now-btn-enterprise">Try Now</button>
+                </div>
               </div>
             </div>
           </div>
