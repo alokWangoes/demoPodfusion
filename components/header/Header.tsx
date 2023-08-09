cbdea2f5d233f4fce1339270daa536e66130479a
aloksamyotech <127102691+aloksamyotech@users.noreply.github.com>
2023-08-09 16:46:34 +0530

    language-select-up-updated

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -2,37 +2,12 @@
 import Link from "next/link";
 import "./Header.css";
 import { useState } from "react";
+import Image from "next/image";
 const Header = () => {
   const [open, setOpen] = useState<any>(false);
   return (
     <>
       <div className="container" style={{ zIndex: "999" }}>
-        <div
-          className="text-right mx-5 "
-          style={{ display: open ? "block" : "none" }}
-        >
-          <div
-
-          // className="languages-header"
-          //   style={{
-          //     margin-left: 801px;
-          //     text-align: center;
-          //     display: block;
-          //     position: fixed;
-          //     color: white;
-          //     margin-top: 107px;
-          //     font-size: 18px;
-          //     opacity: 1.25;
-          //     font-family: INHERIT;
-          //   }}
-          >
-            {/* <span>NL</span>
-            <br />
-            <span>FR</span>
-            <br />
-            <span>DE</span> */}
-          </div>
-        </div>
         <div className="row">
           <div className="col-md-3 col-sm-12 col-xl-3">
             <div className="logo-img">
@@ -50,6 +25,21 @@ const Header = () => {
                 <img src="/Flags.png" alt="flag" />
                 <h4 onClick={() => setOpen(!open)}>EN</h4>
                 <img src="/flat-color-icons_settings.svg" alt="icon-settign" />
+                <div className=" " style={{ display: open ? "block" : "none" }}>
+                  <div className="languages-header">
+                    <div className="inner-language-div">
+                      <img width={18} height={14} src="/flag-1.svg" className="mt-1"  />
+                      <span>NL</span>
+                    </div>
+                    <div className="inner-language-div">
+                      <img  width={18} height={14} src="/flag-2.svg" className="mt-1" />
+                      <span>FR</span>
+                    </div>   <div className="inner-language-div">
+                      <img width={18} height={14} src="/flag-3.svg" className="mt-1"/>
+                      <span>DE</span>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
